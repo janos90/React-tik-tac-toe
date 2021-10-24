617f3c1a307796a6ef700ad62b78223446019407
Juan <garcij07@myunitec.ac.nz>
2021-10-25 03:10:03 +1300

    Display the location for each move in the format (col, row) in the move history list.

--- src/index.js
+++ src/index.js
@@ -48,14 +48,13 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            history: [{squares: Array(9).fill(null),
-            }],
+            history: [{squares: Array(9).fill(null),log: [Array(9).fill(null)]}],
             stepNumber: 0,
             xIsNext: true,
         };
     }
     handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1]
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]){
@@ -63,7 +62,7 @@ class Game extends React.Component {
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
-            history: history.concat([{squares:squares}]),
+            history: history.concat([{squares:squares, log:i+1}]),
             stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         })
@@ -75,14 +74,13 @@ class Game extends React.Component {
         });
     }
   render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber]
-      const winner = calculateWinner(current.squares)
-
+    const history = this.state.history;
+    const current = history[this.state.stepNumber]
+    const winner = calculateWinner(current.squares)
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
-        'Go to game start';
+          'Go to move #' + move + ' ('+ ((history[move].log-1)%3+1)+','+(Math.floor((history[move].log-1)/3)+1)+')'
+          : 'Go to game start';
       return (
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
